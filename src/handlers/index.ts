610f2322c630c0b46a7a20b3be838a9a8a6e7a78
Bryant <bryanttejeda628f@hotmail.com>
2025-03-21 16:40:14 -0400

    handle empty posts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,22 +1,44 @@
 import { Request, Response } from "express"
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
+import slugify from "slugify"
+import { validationResult } from "express-validator"
+
+
 
 export const createAccount = async(req: Request, res: Response) => {
 
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(400).json({errors: errors.array()})
+        return
+    }
+    
     const { email, password } = req.body
 
     const userExist = await User.findOne({email})
-
     if (userExist) {
-        const error = new Error ("El Usuario ya esta registrado")
+        const error = new Error ("Un usuario con ese email ya esta registrado")
+        res.status(409).json({error: error.message})
+        return
+    }
+
+    const handle = slugify(req.body.handle, {
+        lower: true,
+        strict: true,
+        trim: true,
+        replacement: "_"
+    })
+    const handleExist = await User.findOne({handle})
+    if (handleExist) {
+        const error = new Error ("Nombre de usuario no disponible")
         res.status(409).json({error: error.message})
         return
     }
 
     const user = new User(req.body)
-    user.password = await hashPassword(password)
-    
+    user.password = await hashPassword(password)    
+    user.handle = handle
     await user.save()
 
     res.status(201).send('Registro Creado Correctamente')
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,28 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAccount } from "./handlers";
-const router = Router()
 
-router.post("/auth/register", createAccount)
+const router = Router();
 
-export default router
+router.post("/auth/register",
 
+  body("handle")
+    .notEmpty()
+    .withMessage("El handle es obligatorio"),
+
+  body("name")
+  .notEmpty()
+  .withMessage("El nombre es obligatorio"),
+
+  body("email")
+    .isEmail()
+    .withMessage("E-Mail no valido"),
+
+  body("password")
+    .isLength({min:8})
+    .withMessage("Tu Contraseña debe tener almenos 8 caracteres"),
+
+  createAccount
+);
+
+export default router;
