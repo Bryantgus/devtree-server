a33903a221ed82b192de9801dba9ee77a4a15793
Bryant <bryanttejeda628f@hotmail.com>
2025-03-27 16:44:26 -0400

    adding CORS config

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,15 @@ import express from 'express'
 import 'dotenv/config'
 import router from './router'
 import { connectDB } from './config/db'
+import cors from 'cors'
+import { corsConfig } from './config/cors'
 
-const app = express()
 connectDB()
+const app = express()
+
+// Cors
+app.use(cors(corsConfig))
+
 //Leer datos de formularios//
 app.use(express.json())
 app.use('/', router)
