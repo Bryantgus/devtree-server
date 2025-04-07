058ec124827031f78d1652ee6e364c03fe38e4c0
Bryant <bryanttejeda628f@hotmail.com>
2025-04-07 17:05:34 -0400

    model user added links variable

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -71,7 +71,7 @@ export const getUser = async (req: Request, res: Response) => {
 
 export const updateProfile = async (req: Request, res: Response) => {
     try {
-        const { description } = req.body
+        const { description, links } = req.body
         const handle = slugify(req.body.handle, '')
         const handleExists = await User.findOne({handle})
         if (handleExists && handleExists.email !== req.user.email) {
@@ -83,6 +83,7 @@ export const updateProfile = async (req: Request, res: Response) => {
         //Actualizar usuario
         req.user.description = description
         req.user.handle = handle
+        req.user.links = links
         await req.user.save()
         res.send('Perfil Actualizado Correctamente')
 
