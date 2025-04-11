a074872590115a54bf91e6e7258ccbc5df124f7e
Bryant <bryanttejeda628f@hotmail.com>
2025-04-11 13:34:54 -0400

    123

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,22 +1,8 @@
 import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback) {
-        const allowedBaseDomain = 'netlify.app';
-
-        // Permitir sin origin (ej: Postman) si se pasa el flag
-        const allowUndefined = process.argv.includes('--api');
-
-        console.log('🌐 Origin recibido:', origin);
-
-        if (
-            (origin && origin.includes(allowedBaseDomain)) ||
-            (allowUndefined && origin === undefined)
-        ) {
-            callback(null, true);
-        } else {
-            callback(new Error('Error de CORS'));
-        }
+    origin: function (_origin, callback) {
+        callback(null, true); // Permite cualquier origen
     },
     credentials: true,
 };
