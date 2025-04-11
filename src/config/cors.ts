e4822e84feeba1e7e7ccae6c7170bc9e641ed25a
Bryant <bryanttejeda628f@hotmail.com>
2025-04-11 13:27:43 -0400

    adding cors site manually

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions} from 'cors'
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
-        const whiteList = [process.env.FRONTEND_URL]
+        const whiteList = ['https://effortless-blini-cec6a2.netlify.app/']
 
         if(process.argv[2] === '--api') {
             whiteList.push(undefined)
